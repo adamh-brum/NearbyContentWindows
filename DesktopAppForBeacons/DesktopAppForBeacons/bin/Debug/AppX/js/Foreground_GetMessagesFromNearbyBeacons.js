eb294cfac6b528250628d5c6957e86b829bbb830
Adam Haycock <adam.hay.1994@gmail.com>
2017-07-12 11:31:31 +0100

    Removed bin folder (tidy source)

--- DesktopAppForBeacons/DesktopAppForBeacons/bin/Debug/AppX/js/Foreground_GetMessagesFromNearbyBeacons.js
+++
@@ -1,8 +0,0 @@
-﻿var watcher = new Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementWatcher();
-watcher.scanningMode = Windows.Devices.Bluetooth.Advertisement.BluetoothLEScanningMode.active;
-watcher.addEventListener("received", getMessagesForBeacon);
-watcher.start();
-
-function getMessagesForBeacon(args) {
-    console.log(JSON.stringify(args));
-}
