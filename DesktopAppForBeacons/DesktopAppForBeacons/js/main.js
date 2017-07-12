25934dbd08c436246a38d2b46cd1e442d0d49589
Adam Haycock <adam.hay.1994@gmail.com>
2017-07-12 13:06:25 +0100

    API integration proved

--- DesktopAppForBeacons/DesktopAppForBeacons/js/main.js
+++ DesktopAppForBeacons/DesktopAppForBeacons/js/main.js
@@ -1,1 +1,17 @@
-﻿// Your code here!
+﻿window.onload = function () {
+    axios.get("http://nearbycontentapi.azurewebsites.net/api/Content/All").then(res => {
+        console.log('content recieved');
+        var data = [];
+        res.data.forEach(function (element) {
+            data.push({
+                id: element.id,
+                content: element.content,
+                title: element.title,
+                tags: element.tags ? element.tags : []
+            })
+        }, this);
+
+        var element = document.getElementById('stuff');
+        element.innerText = JSON.stringify(data);
+    });
+}
